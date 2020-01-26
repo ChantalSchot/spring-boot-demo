e9d645e0f9064d0341e9e5392646dc3098220e2d
Chantal <chantalschot@outlook.com>
2020-01-26 21:10:38 +0100

    Initial commit

---
+++ src/main/java/nl/home/springbootdemo/Test.java
@@ -0,0 +1,14 @@
+package nl.home.springbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+	
+	@RequestMapping("/test")
+	public String test() {
+		return "Test succeeded!";
+	}
+	
+}
